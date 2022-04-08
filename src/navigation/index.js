236e8ee857fafca6bb1fe76fd11905f65f4baa56
Jose Alfredo Vazquez Jimenez <fredy@MacBook-Air-de-Jose.local>
2022-04-07 22:44:00 -0500

    Se han agregado iconos en el bottom-bar

--- src/navigation/index.js
+++ src/navigation/index.js
@@ -1,4 +1,5 @@
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { Icon } from "react-native-elements";
 
 //Importando Screen
 import { RestaurantScreen } from "./../screens/Restaurant";
@@ -11,12 +12,47 @@ const Tab = createBottomTabNavigator();
 
 export function AppNavigation() {
   return (
-    <Tab.Navigator>
+    <Tab.Navigator
+      screenOptions={({ route }) => ({
+        tabBarActiveTintColor: "#00a680",
+        tabBarInactiveTintColor: "#646464",
+        tabBarIcon: ({ color, size }) =>
+          screenOptionsTabsIcon(route, color, size),
+      })}
+    >
       <Tab.Screen name="Restaurant" component={RestaurantScreen} />
       <Tab.Screen name="Favoritos" component={FavoritosScreen} />
       <Tab.Screen name="Buscador" component={SearchScreen} />
       <Tab.Screen name="Ranking" component={RankingScreen} />
       <Tab.Screen name="Cuenta" component={AccountScreen} />
     </Tab.Navigator>
+  );
+}
+
+function screenOptionsTabsIcon(route, color, size) {
+  let iconName = "";
+
+  if (route.name === "Restaurant") {
+    iconName = "compass-outline";
+  }
+
+  if (route.name === "Favoritos") {
+    iconName = "heart-outline";
+  }
+
+  if (route.name === "Buscador") {
+    iconName = "magnify";
+  }
+
+  if (route.name === "Ranking") {
+    iconName = "star-outline";
+  }
+
+  if (route.name === "Cuenta") {
+    iconName = "account";
+  }
+
+  return (
+    <Icon type="material-community" name={iconName} color={color} size={size} />
   );
 }
